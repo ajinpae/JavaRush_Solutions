0316e7104a07064110428d1b00da34a2316921b9
ajpae <ajpae777@gmail.com>
2025-08-24 22:40:29 +0300

    Implement initial version of the program to display "Amigo is very smart"

---
+++ lvl_1/Solution.java
@@ -0,0 +1,19 @@
+package com.javarush.task.task01.task0142;
+
+/*
+Write a program that displays the message "Amigo is very smart" on the screen.
+
+Requirements:
+• The program must display text.
+• The text must begin with "Amigo".
+• The text must end with "smart".
+• The text must consist of 17 characters, including spaces.
+• The displayed text must correspond to the task.
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        //напишите тут ваш код
+                 System.out.println("Amigo is very smart");
+           }
+}
